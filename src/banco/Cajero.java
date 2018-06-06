44e0796f22d03e4dddd03ddd6f3b6222dfa81e7b
Carlos Ruiz <Carlos Ruiz@DESKTOP-9F11P0Q>
2018-06-06 08:24:36 -0600

    trabajando la clase decimal

---
+++ src/banco/Cajero.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package banco;
+
+/**
+ *
+ * @author Carlos Ruiz
+ */
+public class Cajero {
+    private String nombre;
+
+    public void Cajero(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void procesarCliente(Cliente cliente,Transaccion transaccion){
+    }
+}
